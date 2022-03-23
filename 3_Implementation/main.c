6f469a1eb997426ff8b7a2c0b21475243f55fe99
19wh5a0202-Ujwala <88813499+19wh5a0202-Ujwala@users.noreply.github.com>
2022-03-23 11:56:14 +0530

    Create main.c

---
+++ 3_Implementation/main.c
@@ -0,0 +1,84 @@
+#include"calculator_operations.h"
+
+value compute={6,3};
+int option = 0;
+int n = 1;
+void sel_opt(void);
+int val1=6;
+int val2=2;
+int val3=4;
+/**
+ * @brief GIVING THE OPERATIONS RESPECTIVE VALUES TO FUNTION ITS OPERATION
+ * 
+ */
+enum operations{ add=1, sub, mul, divi,s_root,fact,expo,loge,powe,leave};
+int main(){
+        printf("\nWELCOME\n");            // WELCOME MESSAGE
+
+while(n==1){
+  sel_opt();
+}
+if(n!=1){
+    printf("Thankyou ....!!");
+}
+  
+    }
+    /**
+     * @brief enter the option based on the calculation
+     * 
+     */
+  void sel_opt(){
+
+      printf("Operations\n");
+      printf("\n1.Addition\n2.Subtraction\n3.Multiply\n4.Division\n5.Squareroot\n6.Factorial\n7.Exponent\n8.Logarithm\n9.Power\n10.LEAVE\n");
+      printf("Enter ur option\n");
+      scanf("%d",&option);
+
+      switch(option){
+          case add:
+          addition(&compute);
+          break;
+
+          case sub:
+          subtraction(&compute);
+          break;
+
+          case mul:
+          multiplication(&val1,&val2);
+          break;
+
+          case divi:
+          division(&val1,&val2);
+          break;
+
+          case s_root:
+          squareroot(&val3);
+          break;
+
+          case fact:
+          factorial(&val3);
+          break;
+
+          case expo:
+          exponent(&val3);
+          break;
+
+          case loge:
+          logarithm(&val3);
+          break;
+
+          case powe:
+          power(&val1,&val2);
+          break;
+
+          case leave:
+          exit(0);
+          break;
+
+          default:
+          printf("WRONG OPERATION\n");
+          
+      }
+      printf("enter 1 to continue\n");
+    scanf("%d",&n);
+ }    
